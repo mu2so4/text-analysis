956267588b9af12ead7278697b556156fed9012f
mu2so4 <b4.orangutang@gmail.com>
2023-05-24 14:42:01 +0700

    feat: Добавил извлечение количество глав из произведения

--- src/main/java/ru/nsu/ccfit/muratov/tooi/text/text/downloader/Downloader.java
+++ src/main/java/ru/nsu/ccfit/muratov/tooi/text/text/downloader/Downloader.java
@@ -1,10 +1,16 @@
 package ru.nsu.ccfit.muratov.tooi.text.text.downloader;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.io.InputStreamReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Downloader {
     private final int bookNumber;
@@ -13,17 +19,35 @@ public class Downloader {
         this.bookNumber = bookNumber;
     }
 
-    public String getPage() throws IOException {
+    public int getPage() throws IOException {
         StringBuilder result = new StringBuilder();
         URL url = new URL(String.format("https://ilibrary.ru/text/%d/index.html", bookNumber));
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod("GET");
         try (BufferedReader reader = new BufferedReader(
                 new InputStreamReader(conn.getInputStream()))) {
-            for (String line; (line = reader.readLine()) != null; ) {
+            for (String line; (line = reader.readLine()) != null;) {
                 result.append(line);
             }
         }
-        return result.toString();
+        String response =  result.toString();
+
+        Document doc = Jsoup.parse(response);
+        Elements links = doc.select("a");
+
+        String regex = String.format("/text/%d/p\\.\\d+/index\\.html", bookNumber);
+        Pattern pattern = Pattern.compile(regex);
+        String maxUrl = "";
+        for(var element: links) {
+            String href = element.attr("href");
+            Matcher matcher = pattern.matcher(href);
+            if(matcher.matches()) {
+                maxUrl = href;
+            }
+        }
+
+        maxUrl = maxUrl.replace(Integer.toString(bookNumber), "");
+        maxUrl = maxUrl.replaceAll("\\D", "");
+        return Integer.parseInt(maxUrl);
     }
 }
