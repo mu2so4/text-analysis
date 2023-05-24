f1fbe4ebff63ac5e017afe892bb3f4c01b8dda40
mu2so4 <b4.orangutang@gmail.com>
2023-05-24 15:36:56 +0700

    feat: Add parsing of one chapter

--- src/main/java/ru/nsu/ccfit/muratov/tooi/text/text/downloader/ProseParser.java
+++ src/main/java/ru/nsu/ccfit/muratov/tooi/text/text/downloader/ProseParser.java
@@ -1,5 +1,14 @@
 package ru.nsu.ccfit.muratov.tooi.text.text.downloader;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import ru.nsu.ccfit.muratov.tooi.text.parser.Sentence;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProseParser {
     private final int proseId;
     private final int chapterCount;
@@ -13,5 +22,19 @@ public class ProseParser {
 
     public boolean hasNext() {
         return currentChapterId <= chapterCount;
+    }
+
+    public List<Sentence> nextChapter() throws IOException {
+        String url = String.format("https://ilibrary.ru/text/%d/p.%s/index.html", proseId, currentChapterId);
+        String response = HttpGetClient.getPage(url);
+
+        List<Sentence> sentences = new ArrayList<>();
+        Document doc = Jsoup.parse(response);
+        Element chapter = doc.getElementById("text");
+        for(var paragraph: chapter.select("span")) {
+            sentences.add(new Sentence(paragraph.text()));
+        }
+        currentChapterId++;
+        return sentences;
     }
 }
