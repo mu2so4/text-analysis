594a0a021d56c69a01874e1c1d6c215cbb8cb88b
mu2so4 <b4.orangutang@gmail.com>
2023-05-26 22:03:18 +0700

    feat: Add fetchang sentence length distribution

--- src/main/java/ru/nsu/ccfit/muratov/tooi/text/prose/Prose.java
+++ src/main/java/ru/nsu/ccfit/muratov/tooi/text/prose/Prose.java
@@ -1,6 +1,8 @@
 package ru.nsu.ccfit.muratov.tooi.text.prose;
 
 import com.github.demidko.aot.WordformMeaning;
+import com.google.common.collect.HashMultiset;
+import com.google.common.collect.Multiset;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -9,7 +11,8 @@ import java.util.Map;
 
 public class Prose {
     private final List<Sentence> sentences = new ArrayList<>();
-    private final Map<String, Integer> frequencies = new HashMap<>();
+    private final Map<String, Integer> wordFrequencies = new HashMap<>();
+    private final List<Integer> sentenceLengthFrequencies = new ArrayList<>();
     private int wordCount;
 
     private void addWord(WordformMeaning word) {
@@ -22,15 +25,11 @@ public class Prose {
             addWord(word);
         }
         sentences.add(sentence);
-        wordCount += sentence.getWords().size();
-    }
-
-    public List<Sentence> getSentences() {
-        return sentences;
-    }
-
-    public Map<String, Integer> getFrequencies() {
-        return frequencies;
+        int sentenceSize = sentence.getWords().size();
+        wordCount += sentenceSize;
+        if(sentenceSize != 0) {
+            sentenceLengthFrequencies.add(sentenceSize);
+        }
     }
 
     public int getWordCount() {
@@ -38,8 +37,12 @@ public class Prose {
     }
 
     public List<Map.Entry<String, Integer>> getMostFrequentedWords() {
-        var list = new ArrayList<>(frequencies.entrySet());
+        var list = new ArrayList<>(wordFrequencies.entrySet());
         list.sort((a, b) -> (b.getValue() - a.getValue()));
         return list;
+    }
+
+    public Multiset<Integer> getSentenceLengthFrequencies() {
+        return HashMultiset.create(sentenceLengthFrequencies);
     }
 }
