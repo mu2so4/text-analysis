6ad0a536446ea2a1d2d6c954a91a875fc74cf3a5
mu2so4 <b4.orangutang@gmail.com>
2023-05-27 10:22:35 +0700

    feat: Add getting the z-score

--- src/main/java/ru/nsu/ccfit/muratov/tooi/text/Main.java
+++ src/main/java/ru/nsu/ccfit/muratov/tooi/text/Main.java
@@ -6,7 +6,9 @@ import ru.nsu.ccfit.muratov.tooi.text.prose.Sentence;
 import ru.nsu.ccfit.muratov.tooi.text.prose.parsers.ProseParser;
 import ru.nsu.ccfit.muratov.tooi.text.prose.parsers.ProseTOCParser;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 public class Main {
@@ -34,7 +36,7 @@ public class Main {
                 (double) prose.getAdjectiveCount() / wordCount * 100.);
         System.out.printf("Total functors: %d (%.2f %%)%n", prose.getFunctionWordsCount(),
                 (double) prose.getFunctionWordsCount() / wordCount * 100.);
-        System.out.printf("Sentence count: %d%n", sentenceCount);
+
 
         int topLimit = 100;
         int start = 4;
@@ -47,11 +49,34 @@ public class Main {
         double vocabularyCoefficient = wordCount / coefficient;
         System.out.printf("Vocabulary coefficient: %.3f%n", vocabularyCoefficient);
 
-        System.out.println("\nSentence length distribution");
-        var sentenceDistribution = prose.getSentenceLengthFrequencies();
-        for(var entry: sentenceDistribution.entrySet()) {
-            System.out.printf("%d\t%d\t%.3f%n", entry.getElement(), entry.getCount(),
-                    entry.getCount() / (double) sentenceCount);
+        int sum = 0;
+        for(int index = 0; index < topLimit; index++) {
+            sum += frequencies.get(index).getValue();
+        }
+        double mean = (double) sum / topLimit;
+        double variance = 0;
+        for(int index = 0; index < topLimit; index++) {
+            variance += Math.pow(frequencies.get(index).getValue() - mean, 2);
+        }
+        variance = Math.sqrt(variance);
+        String zScoreFilePath = String.format("%s_z-score.txt", args[0]);
+
+        try(FileOutputStream stream = new FileOutputStream(zScoreFilePath)) {
+            stream.write(String.format("%d%n", topLimit).getBytes(StandardCharsets.UTF_8));
+            for (int index = 0; index < topLimit; index++) {
+                stream.write(String.format("%.5f%n", (frequencies.get(index).getValue() - mean) / variance)
+                        .getBytes(StandardCharsets.UTF_8));
+            }
+        }
+
+        String sentenceDistPath = String.format("%s_sentence_dist.txt", args[0]);
+        try(FileOutputStream stream = new FileOutputStream(sentenceDistPath)) {
+            stream.write(String.format("Sentence count: %d%n", sentenceCount).getBytes(StandardCharsets.UTF_8));
+            var sentenceDistribution = prose.getSentenceLengthFrequencies();
+            for (var entry: sentenceDistribution.entrySet()) {
+                stream.write(String.format("%d\t%d\t%.5f%n", entry.getElement(), entry.getCount(),
+                        entry.getCount() / (double) sentenceCount).getBytes(StandardCharsets.UTF_8));
+            }
         }
     }
 }
