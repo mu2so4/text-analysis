429815445449a62fd7045ff33c01309a893b5455
mu2so4 <b4.orangutang@gmail.com>
2023-05-27 11:13:09 +0700

    feat: Add processing several prose in one time

--- src/main/java/ru/nsu/ccfit/muratov/tooi/text/Main.java
+++ src/main/java/ru/nsu/ccfit/muratov/tooi/text/Main.java
@@ -5,9 +5,24 @@ import ru.nsu.ccfit.muratov.tooi.text.analysis.CommonAnalyzer;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        for(String id: args) {
-            CommonAnalyzer.analyzeProse(Integer.parseInt(id));
+    public static void main(String[] args) {
+        int id;
+        for(String strId: args) {
+            try {
+                id = Integer.parseInt(strId);
+            }
+            catch(NumberFormatException e) {
+                System.err.printf("couldn't parse string '%s' to integer%n", strId);
+                continue;
+            }
+            try {
+                CommonAnalyzer.analyzeProse(id);
+            }
+            catch(IOException e) {
+                System.err.printf("couldn't process prose with id '%s': %s%n", strId, e.getMessage());
+                continue;
+            }
+            System.out.printf("Prose id %s is done%n", strId);
         }
     }
 }
