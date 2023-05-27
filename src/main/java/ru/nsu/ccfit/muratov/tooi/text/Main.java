c08fa7eadbc55335c5b2741e50ace80419985541
mu2so4 <b4.orangutang@gmail.com>
2023-05-27 08:29:14 +0700

    feat: Add regression analysis for vocabulary coef

--- src/main/java/ru/nsu/ccfit/muratov/tooi/text/Main.java
+++ src/main/java/ru/nsu/ccfit/muratov/tooi/text/Main.java
@@ -1,5 +1,6 @@
 package ru.nsu.ccfit.muratov.tooi.text;
 
+import ru.nsu.ccfit.muratov.tooi.text.analysis.ReversedRegressionAnalysis;
 import ru.nsu.ccfit.muratov.tooi.text.prose.Prose;
 import ru.nsu.ccfit.muratov.tooi.text.prose.Sentence;
 import ru.nsu.ccfit.muratov.tooi.text.prose.parsers.ProseParser;
@@ -27,15 +28,21 @@ public class Main {
         System.out.printf("Total nouns: %d%n", prose.getNounCount());
         System.out.printf("Total verbs: %d%n", prose.getVerbCount());
         System.out.printf("Total adjectives: %d%n", prose.getAdjectiveCount());
-        System.out.printf("Total functors: %d%n%n", prose.getFunctionWordsCount());
+        System.out.printf("Total functors: %d%n", prose.getFunctionWordsCount());
 
         int topLimit = 100;
-        System.out.printf("Top %d frequencies%n", topLimit);
-        for(int index = 0; index < topLimit; index++) {
+        int start = 4;
+        ReversedRegressionAnalysis analysis = new ReversedRegressionAnalysis();
+        //System.out.printf("Top %d frequencies%n", topLimit);
+        for(int index = start; index < topLimit; index++) {
             var entry = frequencies.get(index);
+            analysis.add(index + 1., entry.getValue());
             //System.out.printf("'%s': %.3f%n", entry.getKey(), (double) entry.getValue() / wordCount);
-            System.out.printf("%d\t%d%n", index + 1, entry.getValue());
+            //System.out.printf("%d\t%d%n", index + 1, entry.getValue());
         }
+        double coefficient = analysis.getRegression();
+        double vocabularyCoefficient = wordCount / coefficient;
+        System.out.printf("Vocabulary coefficient: %.3f%n", vocabularyCoefficient);
 
         System.out.println("\nSentence length distribution");
         var sentenceDistribution = prose.getSentenceLengthFrequencies();
